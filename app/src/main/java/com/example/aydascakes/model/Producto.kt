f56a6eb3658b26a20f2b39c55cf768fd4af3db67
NormanBetancourth <80001028+NormanBetancourth@users.noreply.github.com>
2023-06-20 01:44:09 -0600

    Add funcionalidades en Firebase

---
+++ app/src/main/java/com/example/aydascakes/model/Producto.kt
@@ -0,0 +1,44 @@
+package com.example.aydascakes.model
+
+import android.annotation.SuppressLint
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.concurrent.CompletableFuture
+
+class Producto(val id : String,
+               val nombre: String,
+               val descripcion: String,
+               val costo: Double,
+               val img : String)
+{
+    override fun toString(): String {
+        return "Producto [id=$id, nombre=$nombre, descripcion=$descripcion, costo=$costo, img=$img]"
+    }
+
+    companion object{
+        @SuppressLint("StaticFieldLeak")
+        private val db = FirebaseFirestore.getInstance()
+        private val CollectionProducto = db.collection("producto")
+
+        fun getProductos(): CompletableFuture<List<Producto>> {
+            val completableFuture = CompletableFuture<List<Producto>>()
+
+            CollectionProducto.get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        var prd = ArrayList<Producto>()
+                        for (document in task.result) {
+                            prd.add(Producto(document.id,
+                                document.get("nombre") as String,
+                                document.get("descripcion") as String,
+                                (document.get("costo") as Long).toDouble(),
+                                document.get("img") as String))
+                        }
+                        completableFuture.complete(prd)
+                    }
+                }
+
+            return completableFuture
+        }
+
+    }
+}
