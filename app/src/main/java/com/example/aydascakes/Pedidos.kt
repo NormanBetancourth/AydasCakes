87e6448d114869734e152397a096f9f7f81fa84f
Carlos Albornoz <er.carlos.albornoz@gmail.com>
2023-06-22 22:51:21 -0600

    Arreglo a los pedidos
    Falta probar al implementar el merge con la actualización del Model de Producto

--- app/src/main/java/com/example/aydascakes/Pedidos.kt
+++ app/src/main/java/com/example/aydascakes/Pedidos.kt
@@ -11,19 +11,22 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NavUtils
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.aydascakes.model.ElementoCarrito
 import com.example.aydascakes.model.Pedido
 import com.example.aydascakes.model.Producto
 import com.example.aydascakes.model.Usuario
 import com.example.aydascakes.service.SessionManager
-import java.util.*
-import kotlin.collections.ArrayList
+import org.w3c.dom.Text
+import kotlin.math.cos
+import kotlin.streams.toList
 
 class Pedidos : AppCompatActivity(){
 
-    lateinit var productosRecyclerView : RecyclerView
-    lateinit var productosArrayList : List<Producto>
+    lateinit var pedidosRecyclerView : RecyclerView
+    lateinit var productosArrayList : List<Pedido>
     lateinit var btnRegresar : ImageButton
     private lateinit var sessionManager: SessionManager
 
@@ -31,94 +34,80 @@ class Pedidos : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pedidos)
+
+        //Iinicializando views
+
         sessionManager = SessionManager(this)
-
-        productosRecyclerView = findViewById(R.id.pedidosLista)
+        pedidosRecyclerView = findViewById(R.id.pedidosLista)
         btnRegresar = findViewById(R.id.btn_regresar)
-        productosRecyclerView.layoutManager = LinearLayoutManager(this)
+        pedidosRecyclerView.layoutManager = LinearLayoutManager(this)
 
-        productosRecyclerView.setHasFixedSize(true)
+        pedidosRecyclerView.setHasFixedSize(true)
 
-        productosArrayList = listOf<Producto>()
+        productosArrayList = listOf<Pedido>()
         getPedidosData()
 
         //Cambia a pantalla de inicio
         btnRegresar.setOnClickListener {
-            val intent: Intent = Intent(this,ModificarDatos::class.java)
-            startActivity(intent)
+            NavUtils.navigateUpFromSameTask(this)
+            finishActivity(0)
         }
     }
     //Obtiene los pedidos del usuario logueado
     private fun getPedidosData(){
         val usuario = sessionManager.obtenerObjeto("usuario", Usuario::class.java) as Usuario
 
-        /*Pedido.getPedidos().
-        thenAccept { pedidos ->
-            pedidos.forEach { pedido ->
-                if(pedido.usuario == usuario.id){
-                    Log.d(ContentValues.TAG, "GET Pedido: $pedido" )
-                    Producto.getProductos()
-                        .thenAccept{productos ->
-                            Log.d(ContentValues.TAG, "GET Productos:${productos}" )
-                            pedido.productos.forEach { elementoCarrito ->
-                                Log.d(ContentValues.TAG, "GET Producto:${elementoCarrito}" )
-                                productos.forEach { producto ->
-                                    if (elementoCarrito.id == producto.id){
-                                         productosArrayList = productos
-                                        Log.d(ContentValues.TAG, "GET Cantidad:${elementoCarrito.cantidad}" )
-                                    }
-                                }
-                            }
-                            productos.forEach { productosRecyclerView.adapter = MyAdapter(productosArrayList)}
-                        }
-                    }
-                }
-            }*/
-
-        Pedido.getPedidos().
-        thenAccept { pedidos ->
-            Log.d(ContentValues.TAG, "GET Pedido: $pedidos" )
-            pedidos.forEach { i ->
-                if(i.usuario == usuario.id){
-                    Producto.getProductos()
-                        .thenAccept { valor ->
-                            Log.d(ContentValues.TAG, "Get Productos: $valor" )
-                            productosArrayList = valor
-                            valor.forEach { productosRecyclerView.adapter = MyAdapter(productosArrayList)}
-                        }
-                }
+        Pedido.getPedidos().thenAccept { listaPedidos ->
+            productosArrayList = listaPedidos.filter { pedido ->
+                pedido.usuario == usuario.id
             }
+            pedidosRecyclerView.adapter = PedidoAdapter(productosArrayList)
         }
     }
-    private inner class MyAdapter(private val productosList : List<Producto>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    private inner class PedidoAdapter(private val productosList : List<Pedido>) : RecyclerView.Adapter<PedidoAdapter.PedidoViewHolder>() {
 
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PedidoViewHolder {
             val itemView = LayoutInflater.from(parent.context).inflate(R.layout.pedido_item,
                 parent, false)
-            return MyViewHolder(itemView)
+            return PedidoViewHolder(itemView)
         }
 
         override fun getItemCount(): Int {
             return productosList.size
         }
 
-        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-
-            val currentites = productosList[position]
-            holder.nomProducto.text = currentites.nombre
-            holder.costo.text = "₡ ${currentites.costo}"
-            //holder.cantidad.text = cantidad
-            val resourceId = resources.getIdentifier(currentites.img, "drawable", packageName)
-            holder.imageView.setImageResource(resourceId)
+        override fun onBindViewHolder(holder: PedidoViewHolder, position: Int) {
+            val pedidoActual = productosList[position]
+            holder.bind(pedidoActual)
         }
 
-        inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        inner class PedidoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+            val fechaPedido : TextView = itemView.findViewById(R.id.tvFechaPedido)
+            val costoTotal : TextView = itemView.findViewById(R.id.tvCostoItemPedido)
+            val cantidad : TextView = itemView.findViewById(R.id.tvCantidad)
 
-            val nomProducto : TextView = itemView.findViewById(R.id.tv_nombreProducto)
-            val costo : TextView = itemView.findViewById(R.id.tv_precio)
-            val imageView: ImageView = itemView.findViewById(R.id.img_producto)
-            val cantidad : TextView = itemView.findViewById(R.id.tv_cantidad)
+            fun bind(pedido : Pedido) {
+
+                val idsProductos = pedido.productos.map { prd -> prd.id }
+
+                Producto.getProductos().thenAccept { productosList ->
+                    fechaPedido.text = pedido.fecha
+
+                    val productosFiltrados = productosList.filter {
+                        producto -> idsProductos.contains(producto.id)
+                    }
+                    var costoTotalProducto : Long = 0L
+                    var contadorProductos = 0
+
+                    productosFiltrados.forEach { producto ->
+                        costoTotalProducto = costoTotalProducto + producto.costo
+                        contadorProductos++
+                    }
+                    costoTotal.text = costoTotal.toString()
+                    cantidad.text = contadorProductos.toString()
+                }
+            }
         }
+
     }
 }
